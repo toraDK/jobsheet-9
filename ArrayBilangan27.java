c2a2bc58c8c692c0809a9ba98d57e647960fde6d
tora180405 <tora@gmail.com>
2023-10-26 12:44:28 +0700

    edit file

---
+++ ArrayBilangan27.java
@@ -0,0 +1,20 @@
+/**
+ * ArrayBilangan27
+ */
+public class ArrayBilangan27 {
+
+    public static void main(String[] args) {
+        int [] bil = new int[4];
+        
+        bil[0] = 5;
+        bil[1] = 12867;
+        bil[2] = 7;
+        bil[3] = 2000000;
+
+
+        for(int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
+        
+    }
+}
